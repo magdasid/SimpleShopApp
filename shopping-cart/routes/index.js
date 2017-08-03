1df19b4669527ec5b044a964b0886854d58e9aef
magdasid <magda_si@wp.pl>
2017-08-03 14:05:34 +0200

    #3 Adding some products to the database

--- shopping-cart/routes/index.js
+++ shopping-cart/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var Product = require('../models/Product');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Shopping Cart' });
+  Product.find(function(err, docs){
+    var productGroup = [];
+    var groupSize = 3;
+    for(var i=0; i < docs.length; i += groupSize) {
+      productGroup.push(docs.slice(i, i+groupSize));
+    }
+    res.render('shop/index', { title: 'Shopping Cart', products: productGroup});
+  });
 });
 
 module.exports = router;
